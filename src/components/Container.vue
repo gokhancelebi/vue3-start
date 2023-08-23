9c8fcf7626133f664f1491a3332925853eab585d
Gokhan <dinatopya03@gmail.com>
2023-08-24 01:52:03 +0300

    ini

---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps(['data'])
+</script>
+<template>
+    <div class="container mx-auto">
+        <slot></slot>
+    </div>
+</template>
+<style scoped>
+.container{
+    max-width: 1150px;
+    width: 100%;
+}
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,10 @@
+<script setup>
+import Container from './Container.vue';
+</script>
 <template>
     <header>
-        <img alt="Vue logo" class="logo" src="./../assets/logo.svg" width="125" height="125" />
+        <Container>
+            <img alt="Vue logo" class="logo" src="./../assets/logo.svg" width="125" height="125" />
+        </Container>
     </header>
 </template>
