3349a8ea3e1a28b00b24214ac1962f1648c3167b
Gokhan <dinatopya03@gmail.com>
2023-08-24 01:55:32 +0300

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Container from './Container.vue';
+</script>
+<template>
+    <footer>
+        <Container>
+            <h1>
+                Footer
+            </h1>
+        </Container>
+    </footer>
+</template>
